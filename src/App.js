978efa2bba968ff2e0d3e26eb81a72d6bcbfffea
rolinmb <66855226+rolinmb@users.noreply.github.com>
2020-06-17 23:55:31 -0500

    update 6/17/2020 11:55 PM

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Paige
+          aho roo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
