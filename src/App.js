6aefaa7d98dbc0c12d623a15bd4331992a8a6963
rolinmb <66855226+rolinmb@users.noreply.github.com>
2020-06-18 16:10:06 -0500

    update 4:09 PM CST 6/16/2020

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 
 function App(){
   return(
-    <div class="App-container">
-		<div class="App-text">
-			Coming soon!
-		</div>
-    </div>
+    <div id="app-wrap">
+	
+	</div>
   );
 }
 
