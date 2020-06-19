6d6b6bda9986d946f39a8a375e0abc980c71ad53
rolinmb <66855226+rolinmb@users.noreply.github.com>
2020-06-19 16:48:11 -0500

    compartmentalizing header & footer into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import { App } from './App.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+	<div id="page-wrap">
+		<div id="header-wrap">
+			<div class="header-content">
+				<p class="header-name">Rolin Blake</p>
+			</div>
+		</div>
+		<App />
+		<div id="footer-wrap">
+			<div id='footer-content'>
+				<p id="footer-links">Links:
+					&nbsp;&nbsp;<a href="https://github.com/rolinmb">GitHub</a>
+					/<a href="https://github.com/rolinmb/fbase-react"> Site Source Code</a>
+					-&nbsp;&nbsp;<a href="https://linkedin.com/in/rolin-blake">LinkedIn</a>
+				</p>
+			</div>
+		</div>
+	</div>
   </React.StrictMode>,
   document.getElementById('index')
 );
